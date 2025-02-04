d957a5303e25ca6701f2a77b8ca3297e4ecf6378
Amos Choo <amoschoouqconnect@gmail.com>
2025-02-04 14:00:53 +1000

    update path to script

--- graph_generation/generate.sh
+++ graph_generation/generate.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
 #SBATCH --job-name=edge
-#SBATCH --time=2:00:00
+#SBATCH --time=4:00:00
 #SBATCH --partition=general
 #SBATCH --account=a_kelvin_tuong
 #SBATCH -e pytorch.error
@@ -15,8 +15,8 @@ source /scratch/project/tcr_ml/gnn_env/bin/activate
 
 # Run the process.py script with actual arguments
 python /scratch/project/tcr_ml/gnn_release/graph_generation/process.py \
-    --root-dir test_data_v2/phs002517  \
-    --directory /scratch/project/tcr_ml/gnn_release/test_data_v2/phs002517/raw \
-    --cancer
+    --root-dir  /scratch/project/tcr_ml/gnn_release/test_data_v2/control_leftovers \
+    --directory /scratch/project/tcr_ml/gnn_release/test_data_v2/control_leftovers/raw \
+    # --cancer
 
 # add/remove cancer flag depending on the dataset class type
